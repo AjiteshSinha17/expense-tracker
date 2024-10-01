2e025e67a5fb3be3983b409e68a45113a66be963
Ajitesh Sinha <ajiteshsinha2004gmail.com>
2024-10-01 15:27:14 +0530

    Overlay

--- lib/widget/ne_expense.dart
+++ lib/widget/ne_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -10,6 +11,17 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  void _datepicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
+
   final _title = TextEditingController();
 
   @override
@@ -18,28 +30,70 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
+  final _amount = TextEditingController();
+
+  void dispoze() {
+    _amount.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
+        padding: const EdgeInsets.all(16),
+        child: Column(children: [
           TextField(
             controller: _title,
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Title")),
           ),
-          Row(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    print(_title.text);
-                  },
-                  child: const Text("Save Expense"))
-            ],
+          Expanded(
+            child: Row(
+              children: [
+                TextField(
+                  controller: _amount,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(
+                        r'^\d+\.?\d{0,2}')), // Allows up to 2 decimal places
+                  ],
+                  maxLength: 5,
+                  decoration: const InputDecoration(
+                      prefixText: '\$', label: Text("Amount")),
+                ),
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text('Select Date'),
+                    IconButton(
+                        onPressed: _datepicker,
+                        icon: const Icon(Icons.calendar_month))
+                  ],
+                ))
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancel')),
+                ElevatedButton(
+                    onPressed: () {
+                      print(_title.text);
+                      print(_amount.text);
+                    },
+                    child: const Text("Save Expense"))
+              ],
+            ),
           )
-        ],
-      ),
-    );
+        ]));
   }
 }
