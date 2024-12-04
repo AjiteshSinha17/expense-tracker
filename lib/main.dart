5375742a07182e6dd299b68c9aa439c957b2d6dd
Ajitesh Sinha <ajiteshsinha2004gmail.com>
2024-12-04 22:36:15 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widget/expenses.dart';
-void main() {
-  runApp(
-     const MaterialApp(
-       
-       home: Expenses(),
-     ));
-}
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 164, 139, 234));
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme), // it apply some default theme of flutter
+    home: const Expenses(),
+  ));
+}
